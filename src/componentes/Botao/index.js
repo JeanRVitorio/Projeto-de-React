38a43cb23f0b12c9df6eb84683dda2cbb93e230b
Jean Ricardo Vitório <112287680+JeanRVitorio@users.noreply.github.com>
2023-04-08 23:31:11 -0300

    Final

--- src/componentes/Botao/index.js
+++ src/componentes/Botao/index.js
@@ -1,12 +1,9 @@
 import './Botao.css'
 
 const Botao = (props) => {
-    return (
-        <button className="botao">
-            {props.children}
-        </button>
-        )
-    }
-    
-    export default Botao
+    return (<button className='botao'>
+        {props.children}
+    </button>)
+}
 
+export default Botao
